ce7e835e3bfef02b23d3c063912d9a05238c0af0
datoxxl <datoxxl@gmail.com>
2014-02-21 20:18:44 +0400

    jQuery update

--- TestCard.Web/App_Start/BundleConfig.cs
+++ TestCard.Web/App_Start/BundleConfig.cs
@@ -9,9 +9,10 @@ namespace TestCard.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.mousewheel.js",
-                        "~/Scripts/jquery.form.js"));
+                        "~/Scripts/jquery-{version}.js"//,
+                        //"~/Scripts/jquery.mousewheel.js",
+                        //"~/Scripts/jquery.form.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -29,6 +30,9 @@ namespace TestCard.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/selectivizr").Include(
+                        "~/Scripts/selectivizr.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/site").Include(
                         "~/Scripts/site.js"));
 
@@ -36,16 +40,16 @@ namespace TestCard.Web
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        //"~/Content/themes/base/jquery.ui.resizable.css",
+                        //"~/Content/themes/base/jquery.ui.selectable.css",
+                        //"~/Content/themes/base/jquery.ui.accordion.css",
+                        //"~/Content/themes/base/jquery.ui.autocomplete.css",
+                        //"~/Content/themes/base/jquery.ui.button.css",
+                        //"~/Content/themes/base/jquery.ui.dialog.css",
+                        //"~/Content/themes/base/jquery.ui.slider.css",
+                        //"~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        //"~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
